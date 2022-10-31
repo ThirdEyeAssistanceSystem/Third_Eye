5aff94a86af6c5e18f375037278c6f7c9eaf14ed
nishat33 <n.t.sithy@gmail.com>
2022-10-31 11:12:47 +0600

    33 commit

---
+++ app/src/main/java/com/example/blindassistancesystem/Dashboard.java
@@ -0,0 +1,286 @@
+package com.example.blindassistancesystem;
+
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.os.Bundle;
+import android.os.PatternMatcher;
+import android.provider.MediaStore;
+import android.speech.RecognizerIntent;
+import android.speech.tts.TextToSpeech;
+import android.telephony.SmsManager;
+import android.text.Html;
+import android.view.GestureDetector;
+import android.view.KeyEvent;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+
+import java.io.IOException;
+import java.security.spec.ECField;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+public class Dashboard extends AppCompatActivity implements GestureDetector.OnGestureListener {
+
+    Button CameraButton;
+    TextToSpeech textToSpeech;
+    ImageButton Call, Emergency, Setting;
+    Button button;
+    String message;
+
+    private float x1, x2, y1, y2;
+    private static int MIN_DISTANCE = 150;
+    public static int REQUEST_CODE;
+    private GestureDetector gestureDetector;
+
+    FusedLocationProviderClient fusedLocationProviderClient;
+
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+        if(REQUEST_CODE==1) {
+            instruction();
+        }
+        this.gestureDetector = new GestureDetector(Dashboard.this, this);
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+        if (ActivityCompat.checkSelfPermission(Dashboard.this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(Dashboard.this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED
+
+        )
+        {
+            getlocation();
+        } else {
+            ActivityCompat.requestPermissions(Dashboard.this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, 44);
+        }
+
+        ActivityCompat.requestPermissions(Dashboard.this, new String[]{Manifest.permission.SEND_SMS},100);
+
+
+        Call = (ImageButton) findViewById(R.id.CallButton);
+        Setting = (ImageButton) findViewById(R.id.Settings);
+        Emergency = (ImageButton) findViewById(R.id.AlertButton);
+        //address = (TextView) findViewById(R.id.address);
+
+
+        Emergency.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sendtext();
+            }
+        });
+
+        CameraButton = (Button) findViewById(R.id.TextButton);
+        CameraButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Dashboard.this, textDetection.class);
+                startActivity(intent);
+            }
+        });
+
+        Setting.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Dashboard.this, Settings.class);
+                startActivity(intent);
+            }
+        });
+
+
+    }
+
+    private void sendtext() {
+
+        String Mobile="+8801748703341";
+        String text=message;
+
+        Intent intent=new Intent(getApplicationContext(),Dashboard.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(getApplicationContext(), 0, intent,0);
+        SmsManager smsManager = SmsManager.getDefault();
+        if(ActivityCompat.checkSelfPermission(Dashboard.this, Manifest.permission.SEND_SMS)==PackageManager.PERMISSION_GRANTED) {
+            smsManager.sendTextMessage(Mobile,null,text,pendingIntent,null);
+            Toast.makeText(Dashboard.this, "Message sent", Toast.LENGTH_LONG).show();
+        }
+        else{
+            Toast.makeText(Dashboard.this, "Nope", Toast.LENGTH_LONG).show();
+        }
+
+       // finish();
+
+    }
+
+    @SuppressLint("MissingPermission")
+    private void getlocation() {
+//        address.setText("hello");
+//
+       fusedLocationProviderClient.getLastLocation().addOnCompleteListener(new OnCompleteListener<Location>() {
+           @Override
+           public void onComplete(@NonNull Task<Location> task) {
+               Location location = task.getResult();
+               if (location != null) {
+
+                   try {
+                       Geocoder geocoder = new Geocoder(Dashboard.this, Locale.getDefault());
+                       List<Address> addresses = geocoder.getFromLocation(location.getLatitude(), location.getLongitude(),1);
+                    message="Send help, I am in danger, I am currently in \n"+ "Locality: "+ addresses.get(0).getLocality()+","+addresses.get(0).getSubLocality()+","+ "\nAddress: " +addresses.get(0).getAddressLine(0);
+                  //address.setText(message);
+                    String phone = "01816579009";
+                   }
+                   catch (IOException e) {
+                       e.printStackTrace();
+                   }
+
+               }
+
+           }
+       });
+    }
+
+
+    @Override
+        public boolean onTouchEvent(MotionEvent event) {
+
+            gestureDetector.onTouchEvent(event);
+            switch (event.getAction()){
+
+                case MotionEvent.ACTION_DOWN:
+                    x1=event.getX();
+                    y1=event.getY();
+                    break;
+                case MotionEvent.ACTION_UP:
+                    x2= event.getX();
+                    y2= event.getY();
+
+                    float valueX= x2-x1;
+                    float valueY=y2-y1;
+                    if(Math.abs(valueY)>MIN_DISTANCE) {
+                        if(y1>y2){
+                            voiceautomation();
+                        }
+                    }
+            }
+        return super.onTouchEvent(event);
+    }
+
+    private void instruction() {
+        textToSpeech = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int i) {
+                if(i==TextToSpeech.SUCCESS){
+                    int lang = textToSpeech.setLanguage(Locale.ENGLISH);
+                    String s = "Swipe up to open voice command";
+                    textToSpeech.setSpeechRate(1.0f);
+                    int speech = textToSpeech.speak(s,TextToSpeech.QUEUE_FLUSH,null);
+                }
+            }
+        });
+    };
+
+
+    private void voiceautomation() {
+        Intent voice = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        voice.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        voice.putExtra(RecognizerIntent.EXTRA_LANGUAGE,
+                Locale.getDefault());
+        voice.putExtra(RecognizerIntent.EXTRA_PROMPT,
+                "What do you want...");
+        startActivityForResult(voice, 1);
+    }
+
+    @Override
+    public boolean dispatchKeyEvent(KeyEvent event) {
+        if(event.getKeyCode()==KeyEvent.KEYCODE_POWER){
+            String Mobile="+8801748703341";
+            String text=message;
+            Intent intent=new Intent(getApplicationContext(),Dashboard.class);
+            PendingIntent pendingIntent = PendingIntent.getActivity(getApplicationContext(), 0, intent,0);
+            SmsManager smsManager = SmsManager.getDefault();
+                smsManager.sendTextMessage(Mobile,null,text,pendingIntent,null);
+                Toast.makeText(Dashboard.this, "Message sent", Toast.LENGTH_LONG).show();
+
+        }
+        return super.dispatchKeyEvent(event);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1 && resultCode == RESULT_OK && data !=null){
+            ArrayList<String> arrayList = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+            if (arrayList.get(0).toString().equals("read")){
+                Intent intent = new Intent(Dashboard.this, textDetection.class);
+                //Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivity(intent);
+            }
+            else if (arrayList.get(0).toString().equals("settings")){
+                Intent intent = new Intent(Dashboard.this, Settings.class);
+                //Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivity(intent);
+            }
+        }
+
+    }
+
+    @Override
+    public boolean onDown(MotionEvent motionEvent) {
+        return false;
+    }
+
+    @Override
+    public void onShowPress(MotionEvent motionEvent) {
+
+    }
+
+    @Override
+    public boolean onSingleTapUp(MotionEvent motionEvent) {
+        return false;
+    }
+
+    @Override
+    public boolean onScroll(MotionEvent motionEvent, MotionEvent motionEvent1, float v, float v1) {
+        return false;
+    }
+
+    @Override
+    public void onLongPress(MotionEvent motionEvent) {
+
+    }
+
+    @Override
+    public boolean onFling(MotionEvent motionEvent, MotionEvent motionEvent1, float v, float v1) {
+        return false;
+    }
+
+
+}
---
+++ app/src/main/java/com/example/blindassistancesystem/Settings.java
@@ -0,0 +1,90 @@
+package com.example.blindassistancesystem;
+
+//import static com.example.blindassistancesystem.Dashboard.ISDISABLE;
+import static com.example.blindassistancesystem.Dashboard.REQUEST_CODE;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SwitchCompat;
+import androidx.core.app.ActivityCompat;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.CompoundButton;
+import android.widget.RelativeLayout;
+import android.widget.Switch;
+import android.widget.TextView;
+
+public class Settings extends AppCompatActivity {
+    RelativeLayout emergency;
+    //Switch aSwitch;
+
+    SwitchCompat switchCompat;
+
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+
+        switchCompat=findViewById(R.id.SoundSwitch);
+        SharedPreferences sharedPreferences=getSharedPreferences("save",MODE_PRIVATE);
+        switchCompat.setChecked(sharedPreferences.getBoolean("value",true));
+        emergency=(RelativeLayout)findViewById(R.id.EmergencyCont);
+        emergency.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Settings.this,EmergencyContact.class);
+                startActivity(intent);
+            }
+        });
+
+        //aSwitch=(Switch)findViewById(R.id.SoundSwitch);
+//        aSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+//            @Override
+//            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+//                if(b){
+//                    REQUEST_CODE = -1;
+//                    aSwitch.setClickable(false);
+//                }
+//                else{
+//                    REQUEST_CODE=1;
+//                    aSwitch.setClickable(true);
+//                }
+//            }
+//        });
+
+        switchCompat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (switchCompat.isChecked())
+                {
+                    // When switch checked
+                    REQUEST_CODE = -1;
+                    SharedPreferences.Editor editor=getSharedPreferences("save",MODE_PRIVATE).edit();
+                    editor.putBoolean("value",true);
+                    editor.apply();
+                    switchCompat.setChecked(true);
+                }
+                else
+                {
+                    // When switch unchecked
+                    REQUEST_CODE=1;
+                    SharedPreferences.Editor editor=getSharedPreferences("save",MODE_PRIVATE).edit();
+                    editor.putBoolean("value",false);
+                    editor.apply();
+                    switchCompat.setChecked(false);
+                }
+            }
+        });
+
+
+    }
+
+
+
+}
